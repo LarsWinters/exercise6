35867f03b05582ccc4998e07c2c29aaefa4c41a2
BenDai98 <benjamin.daiss055@googlemail.com>
2022-06-07 22:08:27 +0200

    Added Tests for chp07 and fixed @before

--- src/test/java/com/practicalunittesting/chp07/EmailTest.java
+++ src/test/java/com/practicalunittesting/chp07/EmailTest.java
@@ -1,8 +1,10 @@
 package com.practicalunittesting.chp07;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class EmailTest {
     /**
@@ -11,12 +13,36 @@ public class EmailTest {
      *
      * @author Tomek Kaczanowski
      */
+    Email mail1;
+    //EmailServer server1;
+    MailClient client1;
+
+    @Before
+    public void init(){
+        mail1 = new Email("abc@def.com","Some Random Title", "Dear All,.......");
+        //server1 = new EmailServer();
+        client1 = new MailClient();
+    }
 
     @Test
     public void createEmailObject() {
-
-
+        //mail1 = new Email("abc@def.com","Some Random Title", "Dear All,.......");
+        assertNotNull(mail1);
+        //assertFalse(null == mail1);
+        assertEquals(Email.class,mail1.getClass());
     }
+
+    @Test
+    public void createClientObject() {
+        assertNotNull(client1);
+        assertEquals(MailClient.class,client1.getClass());
+    }
+
+    @Test
+    public void clientSendEmail() {
+        client1.sendEmail("ppp@uuu.de","TitleTitleTitle", "Hello reciever,...");
+    }
+
 }
 
 
