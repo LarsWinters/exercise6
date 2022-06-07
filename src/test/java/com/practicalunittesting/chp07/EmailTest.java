73711fe24804222f5a6bc00d759dd3eebfccf3da
BenDai98 <benjamin.daiss055@googlemail.com>
2022-06-07 21:30:31 +0200

    Added Test Package chp07

---
+++ src/test/java/com/practicalunittesting/chp07/EmailTest.java
@@ -0,0 +1,22 @@
+package com.practicalunittesting.chp07;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmailTest {
+    /**
+     * Practical Unit Testing with JUnit and Mockito - source code for exercises.
+     * Visit http://practicalunittesting.com for more information.
+     *
+     * @author Tomek Kaczanowski
+     */
+
+    @Test
+    public void createEmailObject() {
+
+
+    }
+}
+
+
